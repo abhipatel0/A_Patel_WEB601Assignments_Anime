b8dc950b111a22eba67b7f6f849aaf1a96550022
Abhi Patel <78019173+abhipatel0@users.noreply.github.com>
2024-02-14 10:06:27 -0500

    final commit

--- A_Patel_Anime/src/app/content-list/content-list.component.ts
+++ A_Patel_Anime/src/app/content-list/content-list.component.ts
@@ -1,20 +1,34 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input, OnInit } from '@angular/core';
 import { Content } from '../helper-files/content-interface';
+import { ContentCardComponent } from '../content-card/content-card.component';
+import { FilterContentPipe } from '../filter-content.pipe';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-content-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ContentCardComponent, FilterContentPipe, FormsModule],
   templateUrl: './content-list.component.html',
   styleUrl: './content-list.component.scss'
 })
 export class ContentListComponent implements OnInit {
-  displayContentInfo(contentItem: Content) {
+  DisplayContentInformation(contentItem: Content) {
     console.log(`ID: ${contentItem.id} and Title: ${contentItem.title}`);
     }
   @Input () contentItems: Content[] = [];
 
+  searchTitle: string = '';
+  contentExists: boolean = false;
+  message: string = '';  
+  selectedTitle: string | null = null;
+
+  checkContentExists() {
+    const foundItem = this.contentItems.find(item => item.title.toLowerCase() === this.searchTitle.toLowerCase());
+    this.contentExists = !!foundItem;
+    this.message = foundItem ? 'Content item exists.' : 'Content item does not exist.';
+    this.selectedTitle = foundItem ? foundItem.title : null;
+  }
   ngOnInit(): void {
     this.contentItems =[
       {
@@ -59,7 +73,7 @@ export class ContentListComponent implements OnInit {
         description:"Death Note (stylized in all caps) is a Japanese manga series written by Tsugumi Ohba and illustrated by Takeshi Obata. It was serialized in Shueisha's shōnen manga magazine Weekly Shōnen Jump from December 2003 to May 2006, with its chapters collected in 12 tankōbon volumes.",
         creator:"Tsugumi Ohba",
         imgURL:"https://upload.wikimedia.org/wikipedia/en/6/6f/Death_Note_Vol_1.jpg",
-        type:"Mystery",
+        type:"Action",
         tags: ["action", "mystery"]
       },
       {
@@ -68,7 +82,7 @@ export class ContentListComponent implements OnInit {
         description:"Tokyo Ghoul (Japanese: 東京喰種トーキョーグール, Hepburn: Tōkyō Gūru) is a Japanese dark fantasy manga series written and illustrated by Sui Ishida. It was serialized in Shueisha's seinen manga magazine Weekly Young Jump from September 2011 to September 2014, with its chapters collected in 14 tankōbon volumes",
         creator:"	Sui Ishida",
         imgURL:"https://upload.wikimedia.org/wikipedia/en/e/e5/Tokyo_Ghoul_volume_1_cover.jpg",
-        type:"Dark fantasy",
+        type:"Action",
         tags: ["action", "dark fantasy"]
       },
       {
